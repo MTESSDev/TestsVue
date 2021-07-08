a50406fd6e5a63ae294f4e7e6d2457183477f421
Dany Cote <infologique@gmail.com>
2021-07-08 19:12:36 -0400

    Convertir le projet .netAppCore en .net WebAPI

---
+++ FRW.SV.GestionFormulaires/Program.cs
@@ -0,0 +1,27 @@
+﻿//using ECS.TR.Commun.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog.Core;
+
+namespace FRW.SV.GestionFormulaires
+{
+    public static class Program
+    {
+        public static LoggingLevelSwitch loggingLevelSwitch => new LoggingLevelSwitch(Serilog.Events.LogEventLevel.Verbose);
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                //config.AddLegacyXmlConverter(hostingContext.HostingEnvironment);
+            })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });//.UseEcsLog(loggingLevelSwitch, false);
+    }
+}
--- FRW.SV.GestionFormulaires/Startup.cs
+++ FRW.SV.GestionFormulaires/Startup.cs
@@ -1,10 +1,137 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Configuration;
+//using CAC.AccesDonnees.Dapper;
+//using CAC.AccesProfil.Client;
+//using ECS.AF.Session;
+//using ECS.DO.ECS1;
+//using ECS.SV.Helpers;
+//using ECS.SV.Helpers.Extensions;
+//using ECS.TR.Commun.Services;
+//using ECS.TR.Contrats;
+//using ECS.TR.Contrats.Contexte;
+using MessagePack;
+using MessagePack.AspNetCoreMvcFormatter;
+using MessagePack.Resolvers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.IISIntegration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace SV.GestionFormulaires
+namespace FRW.SV.GestionFormulaires
 {
-    class Startup
+    public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddDataProtection(options => options.ApplicationDiscriminator = "ECS")
+            //  .AddKeyManagementOptions(options => options.XmlRepository = new ApiXmlRepository(Configuration));
+
+            services.AddHttpContextAccessor();
+            services.AddAuthentication(IISDefaults.AuthenticationScheme);
+            //services.AddScoped<ICodeNT, CodeNTAccesseur>();
+            //services.AddScoped<IDALGeneriqueCAC, DalECS1>();
+            //services.AddProfil(Configuration);
+            //services.AddScoped<IContexte, ContexteSV>();
+            //services.AddScoped<AuthentificationAF>();
+            //services.AddScoped<SessionAF>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowCors",
+                    builder =>
+                    {
+                        var origine = Configuration.GetValue<string?>("AllowedHostsCors");
+                        origine = string.IsNullOrWhiteSpace(origine) ? "*" : origine;
+
+                        if (origine.Equals("*"))
+                        {
+                            if (Configuration.GetValue<bool>("estProduction"))
+                            {
+                                throw new ConfigurationErrorsException("Doit spécifier une valeur différent de '*' pour la config 'AllowedHostsCors'.");
+                            }
+
+                            builder.SetIsOriginAllowed(_ => true);
+                        }
+                        else
+                        {
+                            builder.WithOrigins(origine.Split(";"));
+                        }
+
+                        builder
+                            .WithMethods("GET", "POST")
+                            .AllowCredentials()
+                            .AllowAnyHeader();
+                    });
+            });
+
+            var resolver = CompositeResolver.Create(NativeDateTimeResolver.Instance, ContractlessStandardResolver.Instance);
+            var options = MessagePackSerializerOptions.Standard.WithResolver(resolver);
+            MessagePackSerializer.DefaultOptions = options;
+
+            services.AddControllers(option =>
+            {
+                //option.OutputFormatters.Clear();
+                option.OutputFormatters.Add(new MessagePackOutputFormatter());
+                //option.OutputFormatters.Add(new SystemTextJsonOutputFormatter(new MvcOptions()));
+                //option.InputFormatters.Clear();
+                option.InputFormatters.Add(new MessagePackInputFormatter());
+                //option.InputFormatters.Add(new SystemTextJsonInputFormatter(new MvcOptions()));
+
+            });
+
+            // Ajout d'un document 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ECS.SV.Session",
+                    Version = "v1",
+                    Description = "Service web pour la gestion de la session d'un utilisateur Espace client.",
+                    Contact = new OpenApiContact() { Name = "Équipe STN" }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseEcsExceptionHandler();
+            //app.UseMiddleware<NtlmAndAnonymousSetupMiddleware>();
+
+            app.UseRouting();
+            app.UseAuthorization();
+
+            // Générateur Swagger
+            app.UseSwagger(e => e.SerializeAsV2 = true);
+
+            // Swagger-ui (HTML, JS, CSS, etc.), 
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ECS.SV.Session");
+            });
+
+            app.UseCors("AllowCors");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
 }
